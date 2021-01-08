c22cbad8cba3578bf4fcd8d7c3883d9938b3ae55
Ricardo Rodrigues De Sousa <ricardosousa@cvccorp.com.br>
2021-01-08 18:35:40 -0300

    checkpoint: forms

--- src/app/features/user/pages/register/register.component.ts
+++ src/app/features/user/pages/register/register.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  profileForm = {} as FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      firstName: ['Ricardo', Validators.required],
+      lastName: ['', Validators.minLength(3)],
+      address: this.fb.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zip: [''],
+      }),
+      aliases: this.fb.array([
+        {
+          a: this.fb.control('a'),
+          c: this.fb.control('c'),
+        },
+        {
+          b: this.fb.control('b'),
+        },
+      ]),
+    });
   }
 
+  onSubmit() {
+    console.log('===== onSubmit');
+    console.log(this.profileForm.value);
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ButtonComponent],
-  imports: [CommonModule],
-  exports: [ButtonComponent],
+  declarations: [ButtonComponent, InputComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule],
+  exports: [
+    ButtonComponent,
+    InputComponent,
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
 export class SharedModule {}
