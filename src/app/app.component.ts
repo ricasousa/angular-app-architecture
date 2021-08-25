0142d6426c6e1bdb78e404abd156ee75a26f7c0f
Ricardo Sousa <ricasousa@Ricardos-MacBook-Pro.local>
2021-08-25 14:44:53 -0300

    add snippet to acess DOM element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    @Inject(DOCUMENT) private documentRef: Document,
+    private render: Renderer2) {
+    this.render.addClass(this.documentRef.body, 'some-class');
+  }
+}
